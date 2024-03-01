099df66f91b1071054d42d68a666766843b4e0de
Alexander Ariza <66517893+Alexanderariza@users.noreply.github.com>
2024-03-01 11:39:05 +0100

    Add files via upload

---
+++ org-locations.js
@@ -0,0 +1,78 @@
+var addressPoints = [
+[
+    "<h1>Dr. Emilio Chuvieco</h1> <p><img src='https://flagcdn.com/60x45/es.png' alt='Flag of Spain'> Madrid, España. <center><img src='https://www.redlatif.org/wp-content/uploads/2022/07/emilio2019-Emilio-Chuvieco-300x256.jpg' style='width:150px;'> </center> <ul> <li><a href='https://ebuah.uah.es/dspace/handle/10017/37276' target='_blank'>Catedrático de Análisis Geográfico Regional. Departamento de Geografía Universidad de Alcalá. Tel: 918854438 Fax: (34) 918854439. email: emilio.chuvieco@uah.es</a></li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Empleo de la teledetección y los sistemas de información geográfica para generar y procesar información relevante de cara a la gestión de incendios forestales, con especial énfasis en estimar condiciones de peligro y la evaluación de los efectos del fuego.</li> </ul>",
+    40.4969105,
+    -3.3768897
+],
+
+[
+    "<h1>Dra. Lilia Manzo Delgado</h1> <p><img src='https://flagcdn.com/60x45/mx.png' alt='Flag of Mexico'> México, D.F., México. <center><img src='https://www.redlatif.org/wp-content/uploads/2022/07/20190910_164305-Lilia-Manzo-300x300.jpg' style='width:200px;'> </center> <ul> <li><a href='https://www.unam.mx/' target='_blank'>Laboratorio de Análisis Geoespacial. Instituto de Geografía, Universidad Nacional Autónoma de México. Circuito Investigación Científica, Ciudad Universitaria. C.P. 04510, Coyoacán, México, D.F. Tel. (5255) 5622 4334 ext. 45501, Fax. (5255) 5616 2145 emails: llmanzo@igg.unam.mx, lilia.manzo@gmail.com</a></li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Aplicación y desarrollo de técnicas de percepción remota y SIG para el análisis y monitoreo de los recursos naturales, con énfasis en la fenología de la vegetación, la temperatura de la superficie terrestre, el riesgo de incendios forestales y la identificación de áreas quemadas.</li> </ul>",
+    19.332795,
+    -99.2576481
+],
+
+[
+    "<h1>Ing. Gerardo López Saldaña</h1> <p><img src='https://flagcdn.com/60x45/gb.png' alt='Flag of the United Kingdom'> Reading, Reino Unido. <center><img src='https://www.redlatif.org/wp-content/uploads/2022/07/Gerardo-300x300.jpg' style='width:200px;'> </center> <ul> <li><a href='https://www.reading.ac.uk/meteorology/' target='_blank'>Department of Meteorology University of Reading. Lyle Building – room 508 Whiteknights Reading RG6 6BX UKTel: +44 (0)1183 78 7897</a></li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Sistemas de Alerta temprana.</li> <li>Análisis de series de tiempo.</li> <li>Incendios forestales y área quemada.</li> </ul>",
+    51.4386907,
+    -0.9395725
+],
+
+
+[
+    "<h1>Dr. Wilfrid Schroeder</h1> <p><img src='https://flagcdn.com/60x45/us.png' alt='Flag of the United States'> Maryland, Estados Unidos <center><img src='https://www.redlatif.org/wp-content/uploads/2015/01/Wilfrid.jpg' style='width:150px;'> </center> <ul> <li><a href='https://www.ospo.noaa.gov/' target='_blank'>Investigador del NOAA/NESDIS Center for Satellite Applications and Research, 5200 Auth Road Room 712 Camps Springs MD 20746, USA</a></li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Active fire detection.</li> <li>Biomass burning.</li> <li>Remote sensing.</li> </ul>",
+    38.9721162,
+    -76.9270879
+],
+
+
+[
+    "<h1>Dr. Ivan Csiszar</h1> <p><img src='https://flagcdn.com/60x45/us.png' alt='Flag of the United States'> Maryland, Estados Unidos <center><img src='https://www.redlatif.org/wp-content/uploads/2015/01/Csiszar_I.jpg' style='width:150px;'> </center> <ul> <li><a href='http://www.star.nesdis.noaa.gov/star/index.php' target='_blank'>Investigador en el NOAA/NESDIS Center for Satellite Applications and Research, 5200 Auth Road Room 712 Camps Springs MD 20746, USA</a></li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Active fire detection.</li> <li>Biomass burning.</li> <li>Remote sensing.</li> </ul>",
+    38.9721162,
+    -76.9270879
+],
+
+
+[
+    "<h1>Nicolás A. Mari</h1> <p><img src='https://flagcdn.com/60x45/ar.png' alt='Flag of Argentina'> Cordoba, Argentina. <center><img src='https://media.licdn.com/dms/image/C5603AQEc9exO8G7pwg/profile-displayphoto-shrink_200_200/0/1516567567597?e=1714608000&v=beta&t=AhsIa3QsSkh5PrubyjZzeXyMugv2Kz4e4OawTVLg8B0' style='width:150px;'> </center> <ul><li><a href='https://inta.gob.ar/noticias/cooperacion-cientifica-y-tecnica-entre-argentina-y-colombia' target='_blank'>INTA-Instituto de Clima y Agua (CyA)Tel: (00-54-011) 46210125, Fax: (00-54-011) 462184961email: nmari@cnia.inta.gov.ar</a></li></ul> <h3>Líneas de investigación:</h3> <ul> <li>Aplicaciones de sensores remotos y sistemas de información geográfica para el estudio de la ocurrencia de incendios a nivel regional.</li> <li>Cambios en el uso del suelo.</li></ul>",
+    -31.4174032,
+    -64.1702052
+],
+
+
+[
+    "<h1>Dra. María Isabel Cruz López</h1> <p><img src='https://flagcdn.com/60x45/mx.png' alt='Flag of Mexico'> Mexico DF, Mexico. <center><img src='https://media.licdn.com/dms/image/C5603AQE4Nc4nCuPtSA/profile-displayphoto-shrink_800_800/0/1517449108800?e=1714608000&v=beta&t=aYN8bV_5vtyOVPbuT08ZLdNS0SygmeKsyw0p8820BME' style='width:150px;'></center> <ul> <li><a href='https://www.gob.mx/conabio' target='_blank'>Subcoordinadora de Percepción Remota. Comisión Nacional para el Conocimiento y Uso de la Biodiversidad (CONABIO)Tel: 5004 4946. email: icruz@conabio.gob.mx </a></li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Incendios forestales.</li> <li>Monitoreo de ecosistemas utilizando técnicas de percepción remota.</li> </ul>",
+    19.3013203,
+    -99.1890144
+],
+ 
+[
+    "<h1>Dr. Héctor del Valle</h1> <p><img src='https://flagcdn.com/60x45/ar.png' alt='Flag of Argentina'> Puerto Madryn, Chubut, Argentina. <center><img src='https://www.redlatif.org/wp-content/uploads/2022/07/HFdelValle-Hector-Francisco-del-Valle-267x300.png' style='width:150px;'> </center> <ul> <li><a href='https://www.conicet.gov.ar/' target='_blank'>Investigador en el Instituto Patagónico para el Estudio de Ecosistemas Continentales (IPEEC) Centro Nacional Patagónico (CENPAT) Consejo Nacional de Investigaciones Científicas y Técnicas (CONICET) Boulevard Alte. Brown 2915 (U9120ACF) Puerto Madryn (Chubut) Argentina Teléfono:54-280-4883184, interno 1326 Fax: 54-280-4883543 E-mail: delvalle@cenpat-conicet.gob.ar (laboral) E-mail: hfdelvalle@gmail.com (particular)</a></li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Aplicaciones de los Sensores Remotos al estudio y monitoreo de la degradación de Suelos.</li> <li>Procesamiento avanzado de imágenes de radar de apertura sintética: Polarimetría e Interferometría. Génesis, Clasificación y Cartografía Digital de Suelos.</li> <li>Dinámica de la materia orgánica principalmente en suelos secos.</li> <li>Hidrología superficial (humedales, oasis de riego)</li> </ul>",
+    -42.7847633,
+    -65.011544
+],
+
+[
+    "<h1>Dr. Fabiano Morelli</h1> <p><img src='https://flagcdn.com/60x45/br.png' alt='Flag of Brazil'> Sao Pablo, Brasil. <center><img src='https://www.redlatif.org/wp-content/uploads/2022/07/Fabiano-Morelli-300x300.jpg' style='width:150px;'> </center> <ul> <li><a href='https://www.gov.br/inpe/' target='_blank'>Investigador en el Instituto Nacional de Pesquisas Espaciais. São José dos Campos, Brasil.Tel: (55-12) 3945-6000 Fax: (55-12) 3945 6000email: fmorelli@cptec.inpe.br</a></li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Teledetección</li> <li>Incendios forestales.</li> </ul>",
+    -23.3766526,
+    -46.4215638
+],
+
+[
+    "<h1>MSc. Armando M. Rodriguez Montellano</h1> <h3>Investigador / Actual Coordinador de la RedLaTIF – 2022</h3> <p><img src='https://flagcdn.com/60x45/bo.png' alt='Flag of Bolivia'> Sao Pablo, Brasil. <center><img src='https://www.redlatif.org/wp-content/uploads/2022/07/Armando-Rodriguez-Motellano-2020-cuadrado-300x295.jpg' style='width:150px;'> </center> <ul> <li><a href='http://www.fan-bo.org/' target='_blank'>email: armando.rodriguez.montellano@gmail.com; arodriguez@fan-bo.org Fundación Amigos de la Naturaleza – FAN WEB: https://sites.google.com/site/arodriguezmontellano Blog: http://geoinformacion.wordpress.com Google Academico:http://goo.gl/q2f05Y http://www.mendeley.com/profiles/armando-rodriguez</a></li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Teledetección</li> <li>Incendios forestales.</li>  <li> Detección de áreas quemadas utilizando Google Earth Engine.</li> <li>Monitoreo de alerta de riesgo de incendios forestales (<a href='http://incendios.fan-bo.org/Satrifo/mapa-interactivo/' target='_blank'>SATRIFO</a>) </li>  </ul>",
+    -17.8388215,
+    -63.2412569
+],
+
+[
+    "<h1>Dr. Jesús A. Anaya</h1> <p><img src='https://flagcdn.com/60x45/co.png' alt='Flag of Colombia'> Medellín, Colombia. <center><img src='https://www.redlatif.org/wp-content/uploads/2015/01/janaya-276x300.jpg' style='width:150px;'> </center> <ul> <li>ResearcherID: J-3679-2016 Investigador Universidad de Medellín <a href='https://udemedellin.edu.co/' target='_blank'>https://udemedellin.edu.co/</a> Tel: (574) 3405533 email: janaya@udem.edu.co </li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Teledetección</li> <li>Aplicaciones de la teledetección en estudios relacionados con Hidrología, Vegetación e Incendios. Página personal/web page <a href='https://geomatica.udem.edu.co/flexviewers/landcover_co/index.html' target='_blank'>SERVIDOR DE MAPAS WEB</a>  </ul>",
+    6.2315767,
+    -75.6187332
+],
+
+[
+    "<h1>Dr. German Valencia</h1> <p><img src='https://flagcdn.com/60x45/co.png' alt='Flag of Colombia'> Medellín, Colombia. <center><img src='https://scholar.googleusercontent.com/citations?view_op=view_photo&user=tGt-DOcAAAAJ&citpid=7' style='width:150px;'> </center> <ul> <li>Investigador Universidad de San Buenaventura. Medellín email: german.valencia@usbmed.edu.co <a href='https://www.usbmed.edu.co/' target='_blank'>https://udemedellin.edu.co/</a> Tel: (574) 3405533 email: janaya@udem.edu.co </li> </ul> <h3>Líneas de investigación:</h3> <ul> <li>Teledetección</li> <li>Estimación de emisión de gases efecto invernadero por quema de biomasa  </ul>",
+    6.2489968,
+    -75.5847915
+],
+
+  ]
